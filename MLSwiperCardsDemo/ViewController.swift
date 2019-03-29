1d6abf7a345c3dcfdf4a7c05d973d6b7cdbc10ac
Michel Anderson Lutz Teixeira <michel_lutz@icloud.com>
2019-03-29 11:29:52 -0300

    datasource internal

--- MLSwiperCardsDemo/ViewController.swift
+++ MLSwiperCardsDemo/ViewController.swift
@@ -21,15 +21,14 @@ class CardsViewModel {
 }
 
 class ViewController: UIViewController {
-    private let cellID = "cardCell"
-    private let swiperCards = MLSwiperCards(cellSize: CGSize(width: 312, height: 152))
+    
+    private var swiperCards: MLSwiperCards!
     private let cardsViewModel = CardsViewModel()
     override func viewDidLoad() {
         super.viewDidLoad()
-        swiperCards.delegate = self
-        swiperCards.dataSource = self
-        swiperCards.register(MLSwiperCardCell.self, forCellWithReuseIdentifier: cellID)
         cardsViewModel.loadList()
+        swiperCards = MLSwiperCards(data: cardsViewModel.list, cellSize: CGSize(width: 312, height: 152))
+        swiperCards.translatesAutoresizingMaskIntoConstraints = false
         setupView()
     }
 
@@ -44,31 +43,4 @@ class ViewController: UIViewController {
     }
 }
 
-extension ViewController: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        let itens = cardsViewModel.list.count
-        swiperCards.numberOfPages = itens
-        return itens
-    }
 
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath) as? MLSwiperCardCell else {
-            return UICollectionViewCell()
-        }
-        cell.configure(with: cardsViewModel.list[indexPath.row])
-        cell.prepareForReuse()
-        return cell
-    }
-}
-
-extension ViewController: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-
-        let cellWidth: CGFloat = 312.0 // Your cell width
-
-        let numberOfCells = UIScreen.main.bounds.width / cellWidth
-        let edgeInsets = (UIScreen.main.bounds.width - (numberOfCells * cellWidth)) / (numberOfCells + 1)
-
-        return UIEdgeInsets(top: 0, left: edgeInsets, bottom: 0, right: edgeInsets)
-    }
-}
--- Sources/Core/MLSwiperCards.swift
+++ Sources/Core/MLSwiperCards.swift
@@ -10,8 +10,9 @@ import UIKit
 
 public class MLSwiperCards: UIView {
     public static let name = "MLSwiperCards"
-
-    private var collection: MLSwiperCardsCollection!
+    private let cellID = "cardCell"
+    var data: [MLSwiperCardsModel] = []
+    public private(set) var collection: MLSwiperCardsCollection!
     public var numberOfPages: Int = 0 {
         didSet {
             pageControl.numberOfPages = numberOfPages
@@ -21,18 +22,54 @@ public class MLSwiperCards: UIView {
     private let pageControl: UIPageControl = {
         let pageControl = UIPageControl()
         pageControl.currentPage = 0
-        pageControl.pageIndicatorTintColor = .red
+        pageControl.pageIndicatorTintColor = UIColor.lightGray
+        pageControl.currentPageIndicatorTintColor = UIColor.gray
+        pageControl.translatesAutoresizingMaskIntoConstraints = false
         return pageControl
     }()
 
-    public init(cellSize: CGSize) {
+    public init(data: [MLSwiperCardsModel], cellSize: CGSize, pageConfig: (pageIndicator: UIColor, currentPageIndicator: UIColor)?) {
         super.init(frame: .zero)
+        self.data = data
         collection = MLSwiperCardsCollection(cellSize: cellSize)
+        collection.delegate = self
+        collection.dataSource = self
+        collection.register(MLSwiperCardCell.self, forCellWithReuseIdentifier: cellID)
+        frame = CGRect(x: 0, y: 0, width: cellSize.width, height: cellSize.height)
         setupViewConfiguration()
     }
 
     required public init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension MLSwiperCards: UICollectionViewDataSource {
+    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        let itens = data.count
+        numberOfPages = itens
+        return itens
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath) as? MLSwiperCardCell else {
+            return UICollectionViewCell()
+        }
+        cell.configure(with: data[indexPath.row])
+        cell.prepareForReuse()
+        return cell
+    }
+}
+
+extension MLSwiperCards: UICollectionViewDelegateFlowLayout {
+    private func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        
+        let cellWidth: CGFloat = 312.0 // Your cell width
+        
+        let numberOfCells = UIScreen.main.bounds.width / cellWidth
+        let edgeInsets = (UIScreen.main.bounds.width - (numberOfCells * cellWidth)) / (numberOfCells + 1)
+        
+        return UIEdgeInsets(top: 0, left: edgeInsets, bottom: 0, right: edgeInsets)
     }
 }
 
@@ -44,11 +81,13 @@ extension MLSwiperCards: MLViewConfiguration {
             pageControl.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -8)
         ])
     }
-
+    
     func buildViewHierarchy() {
         addSubview(collection)
         addSubview(pageControl)
     }
-
-
+    
+//    func configureViews() {
+//        backgroundColor = .red
+//    }
 }
